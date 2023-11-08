20ceab9cc7c91547a4593ace1e24b23133a62bc8
fikri <fikribakhtiar26@gmail.com>
2023-11-08 23:10:29 +0700

    add like

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,12 +1,14 @@
 import Detail from '../views/pages/detail';
 import NowPlaying from '../views/pages/now-playing';
 import Upcoming from '../views/pages/upcoming';
+import Like from '../views/pages/like';
 
 const routes = {
   '/': NowPlaying,
   '/now-playing': NowPlaying,
   '/upcoming': Upcoming,
   '/detail/:id': Detail,
+  '/like': Like,
 };
 
 export default routes;
