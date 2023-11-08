f5ca449ed6f9b9c9206df976b5057c33d6f79fb0
fikri <fikribakhtiar26@gmail.com>
2023-11-08 22:59:44 +0700

    add Like

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -41,7 +41,7 @@ const createLikeButtonTemplate = () => `
     <i class="fa fa-heart-o" aria-hidden="true"></i>
   </button>
 `;
- 
+
 const createLikedButtonTemplate = () => `
   <button aria-label="unlike this movie" id="likeButton" class="like">
     <i class="fa fa-heart" aria-hidden="true"></i>
